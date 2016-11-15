9aee9b06f531ceb36c8e5409e047be57012f10dc
utku <utkucagl@gmail.com>
2016-11-15 23:55:26 +0200

    Few changes in FileKeeper ClientSocket and Progress in new Task class namely PushFile

---
+++ nar/lib/Socket/ClientSocket.cpp
@@ -0,0 +1,52 @@
+// Implementation of the ClientSocket class
+
+#include "ClientSocket.h"
+#include "SocketException.h"
+using namespace nar;
+
+ClientSocket::ClientSocket ( std::string host, int port )
+{
+  if ( ! Socket::create() )
+    {
+      throw SocketException ( "Could not create client socket." );
+    }
+
+  if ( ! Socket::connect ( host, port ) )
+    {
+      throw SocketException ( "Could not bind to port." );
+    }
+
+}
+
+
+const ClientSocket& nar::ClientSocket::send ( char * offset, int length ) const
+{
+  if ( ! Socket::send ( offset,length ) )
+    {
+      throw SocketException ( "Could not write to socket." );
+    }
+
+  return *this;
+
+}
+
+const ClientSocket & nar::ClientSocket::recv( char* offset, int length ) const
+{
+	if ( ! Socket::recv ( offset,length ) )
+    {
+      throw SocketException ( "Could not write to socket." );
+    }
+
+  	return *this;
+}
+
+
+// const ClientSocket& ClientSocket::operator >> ( std::string& s ) const
+// {
+//   if ( ! Socket::recv ( s ) )
+//     {
+//       throw SocketException ( "Could not read from socket." );
+//     }
+
+//   return *this;
+// }
