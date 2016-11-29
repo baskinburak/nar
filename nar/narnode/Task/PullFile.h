42e6dd9cbcc92a968ee23e11372426bb37285005
Doguhan Yeke <doguyeke@gmail.com>
2016-11-29 21:34:33 +0300

    Push gets peer list and connects to them

--- nar/narnode/Task/PullFile.h
+++ nar/narnode/Task/PullFile.h
@@ -3,6 +3,12 @@
 
 #include <nar/narnode/Task/ITask.h>
 #include <nar/narnode/global.h>
+#include <nar/lib/Socket/Socket.h>
+#include <nar/lib/rapidjson/document.h>
+#include <nar/lib/rapidjson/allocators.h>
+#include <nar/lib/rapidjson/stringbuffer.h>
+#include <nar/lib/rapidjson/writer.h>
+#include <nar/lib/nlohJson/json.hpp>
 #include <string>
 
 
@@ -11,7 +17,19 @@ namespace nar {
         class PullFile : public nar::task::ITask {
             private:
                 std::string file_name;
+                unsigned long file_size;
+
+
             public:
+                void initialize();
+                void constructRequestJson(nlohmann::json &j, std::string dirname);
+                nar::Socket* connectToServer(nar::Global* globals);
+                void sendRequestJson(nlohmann::json &j, nar::Socket *server_sck);
+                void getResultJson(nlohmann::json &j_resp, nar::Socket *serverSck);
+                void comeTogether(nlohmann::json j_resp, nar::Socket *serverSck);
+                nar::Socket* connectToPeer(std::string peer_ip, int peer_port);
+                void constructJsonforPeer(nlohmann::json &j, std::string dirname);
+
                 PullFile(std::string fn): file_name(fn) { }
                 void run(int unx_sockfd, nar::Global* globals);
         };
