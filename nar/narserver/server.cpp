67850dfca5f746e1846276ddccace10e7fab2747
Fatih Can Kurnaz <webcok@gmail.com>
2016-12-09 18:03:27 +0300

    small fix

--- nar/narserver/server.cpp
+++ nar/narserver/server.cpp
@@ -72,20 +72,7 @@ namespace nar {
             (inf->getSck())->send((char*) response.c_str(), (int)response.size());
             return true;
         }
-        bool get_aes_key(nar::SockInfo* inf, json& jsn) {
 
-			std::string user_name = inf->getAuthenticationHash();
-			nar::User usr = ::db.getUser(user_name);
-			json resp;
-			resp["header"]["channel"] = "sp";
-            resp["header"]["status-code"] = 200;
-            resp["header"]["reply-to"] = "get_aes_request";
-			resp["payload"]["aes"] = usr.cryptedKey;
-			std::cout << "uname: " << usr.user_name << " "  << "key: "<< usr.cryptedKey << std::endl;
-			std::cout << "sname: " << inf->getAuthenticationHash() << std::endl;
-			nar::send_message(*(inf->getSck()), std::string(resp.dump()));
-	   		return true;
-	   }
 		bool getPeerPort(nar::SockInfo* active,std::string token){
 			std::cout << "IMA1" << std::endl;
 			json req;
@@ -539,6 +526,7 @@ namespace nar {
 			nar::send_message(*(inf->getSck()), std::string(resp.dump()));
 	   		return true;
 	   }
+       
     }
 }
 
