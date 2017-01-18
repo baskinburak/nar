10b62390e40059dfa5f48ae7a0c64ef5885813e0
Doguhan Yeke <doguyeke@gmail.com>
2017-01-18 09:53:39 +0300

    compressor.cpp in tests

--- nar/narnode/FileKeeper/FileCompressor.cpp
+++ nar/narnode/FileKeeper/FileCompressor.cpp
@@ -2,7 +2,9 @@
 #include <unistd.h>
 #include <ctime>
 #include <cstdlib>
-#include <nar/narnode/utility.h>
+//#include "../utility.h"
+
+//tODO dogu bu adamin haklari var mi diye bi bak
 
 unsigned long nar::FileCompressor::file_size(char *filename)
 {
@@ -13,17 +15,24 @@ unsigned long nar::FileCompressor::file_size(char *filename)
 	return size;
 }
 
-int nar::FileCompressor::compress_one_file(nar::FileKeeper *myinfile, nar::FileKeeper *myoutfile)
+int nar::FileCompressor::decompress_one_file(char *infilename, char *outfilename)
 {
-    char *infilename = new char[myinfile->getFileName().length() + 1];
-    strcpy(infilename, myinfile->getFileName().c_str());
+	gzFile infile = gzopen(infilename, "rb");
+   	FILE *outfile = fopen(outfilename, "wb");
+   	if (!infile || !outfile) return -1;
+    char buffer[128];
+    int num_read = 0;
+    while ((num_read = gzread(infile, buffer, sizeof(buffer))) > 0) {
+       fwrite(buffer, 1, num_read, outfile);
+       }
 
+    gzclose(infile);
+    fclose(outfile);
+}
 
-    char *outfilename = new char[myoutfile->getFileName().length() + 1];
-    strcpy(outfilename, myoutfile->getFileName().c_str());
-
-
-    FILE *infile = fopen(infilename, "rb");
+int nar::FileCompressor::compress_one_file(char *infilename, char *outfilename)
+{
+	FILE *infile = fopen(infilename, "rb");
     gzFile outfile = gzopen(outfilename, "wb");
     if (!infile || !outfile) return -1;
 
@@ -40,35 +49,4 @@ int nar::FileCompressor::compress_one_file(nar::FileKeeper *myinfile, nar::FileK
     printf("Read %ld bytes, Wrote %ld bytes,Compression factor %4.2f%%\n",
        total_read, file_size(outfilename),
        (1.0-file_size(outfilename)*1.0/total_read)*100.0);
-    delete [] infilename;
-    delete [] outfilename;
 }
-
-int nar::FileCompressor::decompress_one_file(nar::FileKeeper *myinfile, nar::FileKeeper *myoutfile)
-{
-    char *infilename = new char[myinfile->getFileName().length() + 1];
-    strcpy(infilename, myinfile->getFileName().c_str());
-
-
-    char *outfilename = new char[myoutfile->getFileName().length() + 1];
-    strcpy(outfilename, myoutfile->getFileName().c_str());
-
-	gzFile infile = gzopen(infilename, "rb");
-   	FILE *outfile = fopen(outfilename, "wb");
-   	if (!infile || !outfile) return -1;
-    char buffer[128];
-    int num_read = 0;
-    while ((num_read = gzread(infile, buffer, sizeof(buffer))) > 0) {
-       fwrite(buffer, 1, num_read, outfile);
-    }
-    gzclose(infile);
-    fclose(outfile);
-
-    delete [] infilename;
-    delete [] outfilename;
-}
-
-/*
-compress_one_file(file1,file2);
-decompress_one_file(file1,file2);
-*/
