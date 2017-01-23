a6d5bb436d3e83b2be42deffb91ca74ce0378aaa
Baskın Burak Şenbaşlar <basbursen@gmail.com>
2017-01-23 16:45:37 +0300

    hotfix

---
+++ build/CMakeFiles/naripc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/naripc.dir/nar/narnode/IPC/ipcclient.cpp.o"
+  "CMakeFiles/naripc.dir/nar/narnode/IPC/ipcserver.cpp.o"
+  "libnaripc.pdb"
+  "libnaripc.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/naripc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- nar/lib/Socket/USocket.cpp
+++ nar/lib/Socket/USocket.cpp
@@ -19,7 +19,6 @@
 #include <set>
 
 nar::USocket::USocket(unsigned int streamid) {
-  std::cout << "naber lol yaraklar" << std::endl;
   this->stream_id = streamid;
   if((this->udp_sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
     throw "USocket create error.";
