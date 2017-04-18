3ebe99da6752f61fbd2c6eb246159a78d4478df1
utku <utkucagl@gmail.com>
2017-04-18 19:33:58 +0300

    mkdir server

--- nar/narserver/Actions/AuthActions.h
+++ nar/narserver/Actions/AuthActions.h
@@ -7,6 +7,7 @@
 #include <nar/lib/Messaging/MessageTypes/UserRegister.h>
 #include <nar/lib/Messaging/MessageTypes/FilePush.h>
 #include <nar/lib/Messaging/MessageTypes/DirInfo.h>
+#include <nar/lib/Messaging/MessageTypes/Mkdir.h>
 #include <nar/narserver/dbstructs.h>
 
 namespace nar {
@@ -16,6 +17,7 @@ namespace nar {
         void pull_file_action(nar::ServerGlobal* s_global, nar::Socket* skt, nar::MessageTypes::FilePull::Request& req, nar::DBStructs::User&  user);
         void machine_register_action(nar::ServerGlobal* s_global, nar::Socket* skt, nar::MessageTypes::MachineRegister::Request& req, nar::DBStructs::User& user);
         void dir_info_action(nar::ServerGlobal* s_global, nar::Socket* skt, nar::MessageTypes::DirInfo::Request& req, nar::DBStructs::User& user);
+        void mkdir_action(nar::ServerGlobal* s_global, nar::Socket* skt, nar::MessageTypes::Mkdir::Request& req, nar::DBStructs::User& user);
     }
 }
 
