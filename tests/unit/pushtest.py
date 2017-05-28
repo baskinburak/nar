15b0ddee632b3fa037ee0a07337e6a9b95769f31
Fatih Can Kurnaz <webcok@gmail.com>
2017-05-28 19:07:49 +0300

    little change

--- tests/unit/pushtest.py
+++ tests/unit/pushtest.py
@@ -17,17 +17,19 @@ def createfile(filename):
     file.write(id_generator(random.randint(1000,2500000)))
     file.close()
     return stdout_data
+
 def push(filename,username,password):
     p = Popen(['../../build/narcli','push',filename, '/'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
     stdout_data = p.communicate(input=username+'\n'+password+'\n')[0]
     return stdout_data
+
 def deleteFiles():
     p = Popen(['rm',"-rf", 'pushtest/*'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
     stdout_data = p.communicate()[0]
     return stdout_data
 
 if __name__=="__main__":
-    i=0
+    i=22
     make_directory("pushtest")
     while(i<100):
         i+=1
